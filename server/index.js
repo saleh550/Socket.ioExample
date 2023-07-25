6fc1e84ed9694b8f01c124e7e3c3c1be2e97ddf6
סאלח פארס <75793688+saleh550@users.noreply.github.com>
2023-07-25 03:17:55 +0300

    sending a message using socket

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ const io=new Server(server,{
 
 io.on("connection",(socket)=>{
     console.log(`User connected: ${socket.id}`)
+    socket.on("send_message",(data)=>{
+        socket.broadcast.emit("recieve_message",{message:data.message})
+    })
 })
 
 server.listen(3001,()=>{
